096af4969caf3608f7baaa0f23e25b9e1f7e8fe5
golfomania <ml.loeffler@gmx.de>
2018-01-22 23:46:46 +0100

    added code comments

--- reverse_string.js
+++ reverse_string.js
@@ -1,7 +1,12 @@
+//funktion to reverse a string given by parameter
 function reverseString(str) {
+  //split the string into a array with single letters / Split at each "" = everywhere
   str = str.split("");
+  //reverse the order of the array (this is not possible to use direct on a string)
   str = str.reverse();
+  //join the array back to an single string
   str = str.join("");
 
+  //return the reversed string
   return str;
 }
