01cc28c7496826c1dfd017087e33d8532b5d8cb4
Martin Loeffler <ML.Loefflergmx.de>
2018-01-23 20:33:53 +0100

    added comments

--- 06_return_largest_numbers_in_arrays.js
+++ 06_return_largest_numbers_in_arrays.js
@@ -1,10 +1,15 @@
+//function to search the sub elements of an array
+//return an array of the biggest numbers of each array
 function largestOfFour(arr) {
-
+  //iterate through array elements
   for (var i= 0; i<arr.length; i++){
+    //store each sub element to an temporary new array
     var calc = arr[i];
+    //sort numers inside array from big to small
     calc = calc.sort(function(a,b){
       return b-a;
     });
+    //replace sub element with an new element containing only the biggest (=first) number
     arr[i] = calc[0];
   }
   return arr;
