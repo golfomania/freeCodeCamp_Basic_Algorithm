d4d846701952af6729b3885811e21126cf7621a0
Martin Loeffler <ML.Loefflergmx.de>
2018-01-23 15:52:18 +0100

    changend a comment

--- 02_factorialize_a_number.js
+++ 02_factorialize_a_number.js
@@ -1,12 +1,12 @@
 //function to factorialize a number e.g. 5! = 5*4*3*2*1
-function factorialize(num) {
+function factorialize(num){
     //check if given number is zero, then the answer is set to 1
     if (num == 0 ){
     return 1;
     }
 
     //all other numbers where calculated using a for loop
-    //taking care not to decrease till zero
+    //taking care not to decrease i till zero
     else{
     var result = num;
         for (var i=num; i>1;i--){
