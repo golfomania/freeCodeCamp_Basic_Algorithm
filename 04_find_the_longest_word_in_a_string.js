8bf235f75265d843567505a183dc33fe8aa9333b
Martin Loeffler <ML.Loefflergmx.de>
2018-01-23 18:37:01 +0100

    added challenge #4

---
+++ 04_find_the_longest_word_in_a_string.js
@@ -0,0 +1,13 @@
+//function to find the longest word inside a given string and return the lenth of it
+function findLongestWord(str) {
+  //split the string into a array splitted by whithespace
+  str = str.split(" ");
+
+  //sort arry by the length of each array piece, from long to short
+  str.sort(function(a, b) {
+    return b.length - a.length;
+  });
+
+  //return the length of the first array entry, which should now be the one with the biggest length
+  return str[0].length;
+}
