bc61d499fc13e6c5d6a6f6cb8aab25a5b38b92a8
Martin Loeffler <ML.Loefflergmx.de>
2018-01-24 21:08:32 +0100

    added challenge #11

---
+++ 11_slasher_flick.js
@@ -0,0 +1,12 @@
+function slasher(arr, howMany) {
+  return arr.slice(howMany, arr.length);
+}
+
+
+//Same with comments
+//funktion to to give back a given arrays elements staring with the given index
+function slasher(arr, howMany) {
+  //return the sliced portion of the original array
+  //starting / chop the given number of elements till the end of the array
+  return arr.slice(howMany, arr.length);
+}
