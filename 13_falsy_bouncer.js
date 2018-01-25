9f14fb7ea761f9d77b2953cc5815e3b1c634da1c
Martin Loeffler <ML.Loefflergmx.de>
2018-01-25 21:45:52 +0100

    added challenge #13

---
+++ 13_falsy_bouncer.js
@@ -0,0 +1,18 @@
+function bouncer(arr) {
+  return arr.filter(function(el){
+    return Boolean(el);
+  });
+}
+
+
+
+//same with comments
+//function to sort out any falsy (false, null, 0, "", undefined, and NaN)
+//elements out of an given array
+function bouncer(arr) {
+  //returning the filtered array by using a inline callback function, which uses
+  //Boolean() to return true or false depending on the actual element checked by filter()
+  return arr.filter(function(el){
+    return Boolean(el);
+  });
+}
