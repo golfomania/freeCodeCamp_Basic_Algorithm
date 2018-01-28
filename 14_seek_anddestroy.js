109374cc2c0e6da7aa5f023a089393c14039f836
Martin Loeffler <ML.Loeffler@gmx.de>
2018-01-28 23:11:24 +0100

    added comments

--- 14_seek_anddestroy.js
+++ 14_seek_anddestroy.js
@@ -6,3 +6,22 @@ function destroyer() {
   }
   return arr2;
 }
+
+
+
+
+//same with comments
+//function wich gets an array of valus as first argument
+//and then multiple additional arguments wich should be searched and eliminated in the argumnet[0]
+function destroyer() {
+  //assign the argument[0] to a new array arr2
+  var arr2 = arguments[0];
+
+  //iterate with an for loop through the other given arguments
+  for (var i=1;i<arguments.length;i++){
+    //filter out all values with are identical (because filter() keeps all "true checks" we keep all values NOT equal))
+    arr2 = arr2.filter(word => word !== arguments[i]);
+  }
+  //return the filtered array
+  return arr2;
+}
